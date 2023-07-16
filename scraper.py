785a65d96c2496c458465d5a9c20475c2b250d6c
Mike-McKee <mike@dorkydata.com>
2023-07-16 14:33:30 -0400

    fixed scraper function

--- scraper.py
+++ scraper.py
@@ -9,27 +9,29 @@ def update_url(url):
 def scraper(url):
     list_urls = {}
     response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        content_list = soup.find_all('div', class_="collection-item w-dyn-item w-col w-col-6")
+    soup = BeautifulSoup(response.content, 'html.parser')
+    content_list = soup.find_all('div', class_="collection-item w-dyn-item w-col w-col-6")
 
-        for item in content_list:
-            date = item.find('div',class_="piece_date_style")
-            summary = item.find('div',class_="piece_descr_style")
-            title = item.find('div',class_="piece_name_style")
-            link = item.find('a').get('href')
-            # print(date.text.strip(), title.text.strip(), summary.text.strip(),update_url(url) + link, sep=' ')
-            for j in range(1,len(content_list) + 1):
-                list_urls[j] = {
-                                'Title': title.text.strip(),
-                                'Summary': summary.text.strip(),
-                                'Link': update_url(url) + link,
-                                'Date': date.text.strip()
-                                }
-                
-        return pprint.pprint(list_urls)
+    num = [i for i in range(1,len(content_list) + 1)]
+
+    #TODO: figure out how to order dictionary so that we start with the oldest blog posts first
+
+    for item in content_list:
+        date = item.find('div',class_="piece_date_style")
+        summary = item.find('div',class_="piece_descr_style")
+        title = item.find('div',class_="piece_name_style")
+        link = item.find('a').get('href')
+        list_urls[num[0]] = {
+                        'Title': title.text.strip(),
+                        'Summary': summary.text.strip(),
+                        'Link': update_url(url) + link,
+                        'Date': date.text.strip()
+                        }
+        num.pop(0)
             
-    else:
-        return "No Data"
-    
-print(scraper('https://www.dorkydata.com/dorky-blog'))
+    return pprint.pprint(list_urls)
+
+def main(url):
+    return scraper(url)
+
+print(main('https://www.dorkydata.com/dorky-blog'))
