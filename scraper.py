9d7867791e4e9b0a9714382c8b490ad7ae818801
Mike-McKee <mike@dorkydata.com>
2023-07-16 14:56:11 -0400

    optimized scraper function w/enumerate

--- scraper.py
+++ scraper.py
@@ -12,22 +12,19 @@ def scraper(url):
     soup = BeautifulSoup(response.content, 'html.parser')
     content_list = soup.find_all('div', class_="collection-item w-dyn-item w-col w-col-6")
 
-    num = [i for i in range(1,len(content_list) + 1)]
-
     #TODO: figure out how to order dictionary so that we start with the oldest blog posts first
 
-    for item in content_list:
+    for index, item in enumerate(content_list,start=1):
         date = item.find('div',class_="piece_date_style")
         summary = item.find('div',class_="piece_descr_style")
         title = item.find('div',class_="piece_name_style")
         link = item.find('a').get('href')
-        list_urls[num[0]] = {
+        list_urls[index] = {
                         'Title': title.text.strip(),
                         'Summary': summary.text.strip(),
                         'Link': update_url(url) + link,
                         'Date': date.text.strip()
                         }
-        num.pop(0)
             
     return pprint.pprint(list_urls)
 
